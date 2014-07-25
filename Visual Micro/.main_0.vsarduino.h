ad9fe0d37d7041d79448b127db9d1deede828824
Gordon Bailey <gb@gordonbailey.net>
2014-07-24 22:41:10 -0400

    Motor speed testing

--- Visual Micro/.main_0.vsarduino.h
+++ Visual Micro/.main_0.vsarduino.h
@@ -41,14 +41,12 @@ extern "C" void __cxa_pure_virtual() {;}
 
 //
 //
-void receiveEvent(int numBytes);
-void requestEvent();
 
 #include "C:\Program Files (x86)\Arduino\hardware\arduino\cores\arduino\arduino.h"
 #include "C:\Program Files (x86)\Arduino\hardware\arduino\variants\standard\pins_arduino.h" 
-#include "C:\Users\gordon\Documents\main_0\main_0.ino"
-#include "C:\Users\gordon\Documents\main_0\led.cpp"
-#include "C:\Users\gordon\Documents\main_0\led.h"
-#include "C:\Users\gordon\Documents\main_0\motor.cpp"
-#include "C:\Users\gordon\Documents\main_0\motor.h"
+#include "C:\Users\gordon\Documents\bar-bot\main_0\main_0.ino"
+#include "C:\Users\gordon\Documents\bar-bot\main_0\led.cpp"
+#include "C:\Users\gordon\Documents\bar-bot\main_0\led.h"
+#include "C:\Users\gordon\Documents\bar-bot\main_0\motor.cpp"
+#include "C:\Users\gordon\Documents\bar-bot\main_0\motor.h"
 #endif
